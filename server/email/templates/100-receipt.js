803f946e76612c641812ba82ad46f7a035773d80
Tuomas Karjalainen <tuomas.karj@outlook.com>
2017-08-12 14:16:14 +0300

    Fixes to email system

--- server/email/templates/100-receipt.js
+++ server/email/templates/100-receipt.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const BaseTemplate = require('./Template')
 const db = require('./../../db')
 const crypto = require('crypto');
@@ -13,7 +14,7 @@ Hei {{ user.firstname }}!
 Tästä linkistä voit ladata itsellesi kuitin asioinnistasi KirjatKiertoon.fi -palvelussa:
 {{ link }}
 
-Linkki on voimassa 30 päivän ajan, jonka jälkeen tositetta ei ole enää mahdollista ladata. Säilytystä varten tallenna tiedosto itsellesi.
+Linkki on voimassa 60 päivän ajan, jonka jälkeen tositetta ei ole enää mahdollista ladata. Säilytystä varten tallenna tiedosto itsellesi.
 
 -  -  -  -  -  -  -  -  -
 
@@ -34,7 +35,7 @@ Tästä linkistä voit ladata itsellesi kuitin asioinnistasi KirjatKiertoon.fi -
 <a href="{{ link }}">{{ link }}</a>
 </p>
 <p>
-Linkki on voimassa 30 päivän ajan, jonka jälkeen tositetta ei ole enää mahdollista ladata. Säilytystä varten tallenna tiedosto itsellesi.
+Linkki on voimassa 60 päivän ajan, jonka jälkeen tositetta ei ole enää mahdollista ladata. Säilytystä varten tallenna tiedosto itsellesi.
 </p>
 <hr />
 <p>
@@ -63,7 +64,7 @@ Tuomas Karjalainen<br>
       .then(() => db.query('INSERT INTO receipttokens (receipt, content) VALUES ($1, $2) RETURNING content', [this._object, token]))
       .then(res => {
         if (res != null && res.rowCount > 0) {
-          this._data.link = 'http://localhost:8080/api/receipt/' + res.rows[0].content
+          this._data.link = process.env.EMAIL_LINK_DOMAIN + '/api/receipt/' + res.rows[0].content
         }
       })
       .catch(err => {
